8542c41784be7ae2af68c497f4bb7e2fe8c2c1a0
Matteo <mattegiorgi@gmail.com>
2026-08-17 11:03:44 +0200

    mark 'asteroids' repo

--- haunt.scm
+++ haunt.scm
@@ -73,7 +73,7 @@
     (make-repo "vim-notewiki"           "vim plugin for note-taking"                   "Vim Script"       "#199f4b")
     (make-repo "vim-startscreen"        "vim plugin for splash-screen"                 "Vim Script"       "#199f4b")
     (make-repo "wiener"                 "Wiener's attack on RSA"                       "Wolfram Language" "#dd1100")
-    (make-repo "asteroids"              "modern implementation of Asteroids"           "JavaScript"       "#f1e05a")
+    (make-repo "asteroids"              "modern implementation of Asteroids"           "JavaScript"       "#f1e05a" #t)
     (make-repo "funint"                 "functional interpreter"                       "OCaml"            "#ef7a08")
     (make-repo "graph"                  "generic objects graph library"                "Java"             "#b07219")
     (make-repo "membox"                 "object repository concurrent server"          "C"                "#555555")
